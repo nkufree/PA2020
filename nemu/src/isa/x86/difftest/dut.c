51e22f7d84cbd60d64260bf70834d0b9c6563090
tracer-ics2020 <tracer@njuics.org>
2024-03-04 11:55:34 +0000

    >  compile 2110853 朱子轩 Linux 017810e76610 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:55:33 up  1:49,  0 users,  load average: 0.51, 0.37, 0.22

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -4,7 +4,16 @@
 #include "difftest.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool ret = true;
+  for(int i = 0; i < 8; i++)
+  {
+    if(ref_r->gpr[i]._32 != cpu.gpr[i]._32)
+    {
+      printf("[reg error]  reg %s exspect: %08x, but %08x", reg_name(i, 4), ref_r->gpr[i]._32, cpu.gpr[i]._32);
+    }
+    ret = false;
+  }
+  return ret;
 }
 
 void isa_difftest_attach() {
