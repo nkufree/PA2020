77d3df9f978694893750f7d38da93d114f13f95a
tracer-ics2020 <tracer@njuics.org>
2024-03-05 08:00:59 +0000

    >  compile 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  08:00:58 up  1:56,  0 users,  load average: 0.25, 0.17, 0.18

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -64,10 +64,10 @@ static inline def_rtl(is_add_carry, rtlreg_t* dest,
 
 #define def_rtl_setget_eflags(f) \
   static inline def_rtl(concat(set_, f), const rtlreg_t* src) { \
-    TODO(); \
+    set_flag(concat(F_, f), *src); \
   } \
   static inline def_rtl(concat(get_, f), rtlreg_t* dest) { \
-    TODO(); \
+    *dest = get_flag(concat(F_, f)); \
   }
 
 def_rtl_setget_eflags(CF)
