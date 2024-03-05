84c72b7f82bc712a276aaa132bf83fc66ce1b5b8
tracer-ics2020 <tracer@njuics.org>
2024-03-05 09:36:16 +0000

    >  compile 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  09:36:15 up  3:32,  0 users,  load average: 0.18, 0.13, 0.15

--- nemu/src/isa/x86/exec/cc.h
+++ nemu/src/isa/x86/exec/cc.h
@@ -32,6 +32,8 @@ static inline void rtl_setcc(DecodeExecState *s, rtlreg_t* dest, uint32_t subcod
     case CC_O:
     case CC_B:
     case CC_E:
+      rtl_get_ZF(s, dest);
+      break;
     case CC_BE:
     case CC_S:
     case CC_L:
