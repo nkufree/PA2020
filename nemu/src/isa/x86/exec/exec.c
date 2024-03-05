1ed42089d864de96cefc1182286973fe3343c8d8
tracer-ics2020 <tracer@njuics.org>
2024-03-05 07:14:07 +0000

    >  compile 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  07:14:07 up  1:09,  0 users,  load average: 0.77, 0.40, 0.24

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -140,6 +140,7 @@ again:
     IDEXW(0xd2, gp2_cl2E, gp2, 1)
     IDEX (0xd3, gp2_cl2E, gp2)
     EX   (0xd6, nemu_trap)
+    IDEXW(0xeb, J, jmp, 1)
     IDEX (0xe8, call_SI, call)
     IDEXW(0xf6, E, gp3, 1)
     IDEX (0xf7, E, gp3)
