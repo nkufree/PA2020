cfb838172e92e6b47f9aa697e74c940598be003d
tracer-ics2020 <tracer@njuics.org>
2024-03-05 15:58:03 +0000

    >  compile 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:58:02 up  3:23,  0 users,  load average: 0.26, 0.12, 0.07

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -27,20 +27,13 @@ static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  uint32_t sign = ((*src1 >> (width * 8 - 1)) & 0x1);
-  if(sign == 0)
-  {
-    *dest = *src1;
-  }
-  else
-  {
-    *dest = (0xffffffffu << (width * 8)) | *src1;
-  }
+  int32_t val = *src1;
+  *dest = (val << (32 - width * 8)) >> (32 - width * 8);
 }
 
 static inline def_rtl(zext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- zeroext(src1[(width * 8 - 1) .. 0])
-  *dest = *src1;
+  *dest = (*src1 << (32 - width * 8)) >> (32 - width * 8);
 }
 
 static inline def_rtl(msb, rtlreg_t* dest, const rtlreg_t* src1, int width) {
