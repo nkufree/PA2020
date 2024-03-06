c2c3dfca3ebaea534e199b08aefe383f76356a06
tracer-ics2020 <tracer@njuics.org>
2024-03-06 16:11:08 +0000

    >  compile 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  16:11:08 up  4:14,  0 users,  load average: 0.44, 0.26, 0.22

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -41,9 +41,9 @@ void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
   uint32_t sync = vgactl_port_base[1];
+  printf("sync = %d\n", sync);
   if(sync == 1)
   {
-    printf("update\n");
     update_screen();
     vgactl_port_base[1] = 0;
   }
