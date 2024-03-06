1bc5d041a33c49cbbc08d7fb2594d9ca4ffb4c4e
tracer-ics2020 <tracer@njuics.org>
2024-03-06 02:23:01 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  02:23:00 up  1:25,  0 users,  load average: 0.03, 0.15, 0.16

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,6 +14,62 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  int d;
+  char* s;
+  va_start(ap, fmt);
+  int out_index = 0;
+  while (*fmt)
+  {
+    if(*fmt != '%')
+    {
+      out[out_index] = *fmt;
+      out_index++;
+    }
+    else
+    {
+      fmt++;
+      switch(*fmt)
+      {
+        case 'c':
+          out[out_index] = va_arg(ap, int);
+          out_index++;
+          break;
+        case 'd':
+          d = va_arg(ap, int);
+          if(d < 0)
+          {
+            out[out_index] = '-';
+            out_index++;
+            d = -d;
+          }
+          if(d == 0)
+          {
+            out[out_index] = '0';
+            out_index++;
+            break;
+          }
+          while (d != 0)
+          {
+            int tmp = d % 10;
+            d = d / 10;
+            out[out_index] = tmp + '0';
+            out_index++;
+          }
+          break;
+        case 's':
+          s = va_arg(ap, char*);
+          d = strlen(s);
+          strncpy(out + out_index, s, d);
+          out_index += d;
+          break;
+        default:
+          assert(0);
+      }
+    }
+    fmt++;
+  }
+  
   return 0;
 }
 
