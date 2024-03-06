e3d99d7b59219a3d7977cd1a91db73772955ce85
tracer-ics2020 <tracer@njuics.org>
2024-03-06 14:48:42 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  14:48:42 up  2:52,  0 users,  load average: 0.22, 0.22, 0.13

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -2,6 +2,8 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define W 400
+#define H 300
 
 void __am_gpu_init() {
 }
@@ -15,6 +17,18 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *tmp = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t (*fb)[W] = (uint32_t (*)[W])(uintptr_t)tmp;
+  uint32_t (*pixels)[W] = ctl->pixels;
+  int rw = ctl->x + ctl->w > W ? W : ctl->x + ctl->w;
+  int bh = ctl->y + ctl->h > H ? H : ctl->y + ctl->h;
+  for(int x = ctl->x; x < rw; x++)
+  {
+    for(int y = ctl->y; y < bh; y++)
+    {
+      fb[y][x] = pixels[y - ctl->h][x - ctl->x];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
