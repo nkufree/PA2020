59e988e745a89713e580c263b02d33e09937fb42
tracer-ics2020 <tracer@njuics.org>
2024-03-06 15:34:04 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:34:04 up  3:37,  0 users,  load average: 0.40, 0.34, 0.38

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -52,7 +52,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
       if(y+i < H&&x+j <W)
       {
         fb[W*(y+i)+x+j] = pixels[w*i+j];
-        printf("fb addr: %d", fb + W*(y+i)+x+j);
+        printf("fb addr: %d\n", fb + W*(y+i)+x+j);
         //printf("%x\n",fb[W*(y+i)+x+j]);
       }
     }
