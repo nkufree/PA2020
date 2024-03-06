b479c1e2cbfb00351f1426c2efe39932a5e642f2
tracer-ics2020 <tracer@njuics.org>
2024-03-06 08:52:47 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  08:52:47 up  7:55,  0 users,  load average: 0.33, 0.18, 0.14

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -116,74 +116,6 @@ int printf(const char *fmt, ...) {
   return 0;
 }
 
-// int vsprintf(char *out, const char *fmt, va_list ap) {
-//   int d;
-//   char* s;
-//   int out_index = 0;
-//   while (*fmt)
-//   {
-//     if(*fmt != '%')
-//     {
-//       out[out_index] = *fmt;
-//       out_index++;
-//     }
-//     else
-//     {
-//       fmt++;
-//       switch(*fmt)
-//       {
-//         case 'c':
-//           out[out_index] = va_arg(ap, int);
-//           out_index++;
-//           break;
-//         case 'd':
-//           d = va_arg(ap, int);
-//           if(d < 0)
-//           {
-//             out[out_index] = '-';
-//             out_index++;
-//             d = -d;
-//           }
-//           if(d == 0)
-//           {
-//             out[out_index] = '0';
-//             out_index++;
-//             break;
-//           }
-//           int len = 0;
-//           int dc = d;
-//           while (dc != 0)
-//           {
-//               len++;
-//               dc = dc / 10;
-//           }
-//           len += out_index - 1;
-//           int tmp;
-//           while (d != 0)
-//           {
-//             tmp = d % 10;
-//             d = d / 10;
-//             out[len] = tmp + '0';
-//             len--;
-//             out_index++;
-//           }
-//           break;
-//         case 's':
-//           s = va_arg(ap, char*);
-//           d = strlen(s);
-//           strncpy(out + out_index, s, d);
-//           out_index += d;
-//           break;
-//         default:
-//           assert(0);
-//       }
-//     }
-//     fmt++;
-//   }
-//   out[out_index] = '\0';
-//   return 0;
-// }
-
 int vsprintf(char *out, const char *fmt, va_list ap) {
   fmt_parser fmtp = {fmt, ap, false};
   while (!fmtp.end)
@@ -203,10 +135,22 @@ int sprintf(char *out, const char *fmt, ...) {
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  fmt_parser fmtp = {fmt, ap, false};
+  int len = 0;
+  while (!fmtp.end && len < n)
+  {
+    *out = get_char(&fmtp);
+    out++;
+    len++;
+  }
   return 0;
 }
 
