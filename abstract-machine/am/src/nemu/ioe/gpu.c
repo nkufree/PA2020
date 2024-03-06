1227174d35464c6ca7a8115816501921abffbdcb
tracer-ics2020 <tracer@njuics.org>
2024-03-06 15:16:37 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:16:36 up  3:20,  0 users,  load average: 0.56, 0.75, 0.58

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -49,9 +49,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   //       //printf("%x\n",fb[W*(y+i)+x+j]);
   //     }
   //   }
-  // if (ctl->sync) {
-  //   outl(SYNC_ADDR, 1);
-  // }
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
