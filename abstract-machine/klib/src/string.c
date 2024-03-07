a5d7f9d90fcf571cc27ed5966cf0b8e1cdc5d16c
tracer-ics2020 <tracer@njuics.org>
2024-03-07 09:05:05 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  09:05:05 up  3:36,  0 users,  load average: 0.07, 0.16, 0.19

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -93,7 +93,7 @@ void* memmove(void* dst,const void* src,size_t n) {
     dst++;
     src++;
   }
-  return dst;
+  return ret;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
@@ -131,7 +131,7 @@ int memcmp(const void* s1, const void* s2, size_t n) {
     s1++;
     s2++;
   }
-  return 0;
+  return *(char*)s1 - *(char*)s2;
 }
 
 #endif
