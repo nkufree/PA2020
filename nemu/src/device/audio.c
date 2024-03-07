bee6b800a74e60c57c06ffb017cb09dc4b03a77e
tracer-ics2020 <tracer@njuics.org>
2024-03-07 09:49:14 +0000

    >  compile 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  09:49:14 up  4:21,  0 users,  load average: 0.40, 0.26, 0.19

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -33,7 +33,8 @@ static inline void audio_play(void *userdata, uint8_t *stream, int len) {
   if(free < len) 
 		nread = free;
   memcpy(stream, sbuf + head, nread);
-	printf("num2: %d\n", head + nread);
+	printf("audio_base[reg_count]: %d, nread: %d\n", audio_base[reg_count], nread);
+	// printf("num2: %d\n", head + nread);
 	// printf("recv: %d\n", *stream);
   audio_base[reg_count] -= nread;
 	head += nread;
