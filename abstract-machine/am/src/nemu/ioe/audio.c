52961792fe384b11c77cfccb526a74cde11d477a
tracer-ics2020 <tracer@njuics.org>
2024-03-07 06:32:04 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  06:32:03 up  1:03,  0 users,  load average: 0.25, 0.18, 0.18

--- abstract-machine/am/src/nemu/ioe/audio.c
+++ abstract-machine/am/src/nemu/ioe/audio.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <nemu.h>
-#include <string.h>
+#include <klib.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -35,10 +35,12 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   while(len > 0)
   {
     int nwrite = len > buf_size ? buf_size : len;
+    printf("1\n");
     while (inl(AUDIO_COUNT_ADDR) != 0);
     memcpy((uint32_t*)(uintptr_t)AUDIO_SBUF_ADDR, ctl->buf.start, nwrite);
     outl(AUDIO_COUNT_ADDR, nwrite);
     len -= nwrite;
+    printf("10\n");
   }
   
 }
