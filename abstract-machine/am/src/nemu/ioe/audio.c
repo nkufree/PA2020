9cd0fb0cc82a064d8174c02b93b0c74183f23e01
tracer-ics2020 <tracer@njuics.org>
2024-03-07 09:44:33 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  09:44:32 up  4:16,  0 users,  load average: 0.45, 0.26, 0.17

--- abstract-machine/am/src/nemu/ioe/audio.c
+++ abstract-machine/am/src/nemu/ioe/audio.c
@@ -33,7 +33,6 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  printf("buf_size: %d\n", buf_size);
   int len = ctl->buf.end - ctl->buf.start;
   void* start = ctl->buf.start;
   while(len > 0)
@@ -41,7 +40,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
     int free = inl(AUDIO_COUNT_ADDR) < head ? buf_size - head : buf_size - inl(AUDIO_COUNT_ADDR);
     int nwrite = len < free ? len : free;
     // printf("in: %d\n", *(uint8_t*)start);
-    // printf("head: %d, num: %d\n", head, head + nwrite);
+    printf("head: %d, num: %d, free: %d, len: %d\n", head, head + nwrite, free, len);
     memcpy((uint32_t*)(uintptr_t)(AUDIO_SBUF_ADDR + head), start, nwrite);
     head = nwrite + head;
     if(head >= buf_size)
