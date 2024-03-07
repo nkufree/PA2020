7831ca7face2bb7ed3b7b37f4876ff2c524e4ba0
tracer-ics2020 <tracer@njuics.org>
2024-03-07 03:41:46 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  03:41:45 up 8 min,  0 users,  load average: 0.06, 0.21, 0.14

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -6,13 +6,13 @@ uint64_t start_time;
 void __am_timer_init() {
   uint64_t low = inl(RTC_ADDR);
   uint64_t high = inl(RTC_ADDR + 4);
-  start_time = (high << 32) + low;
+  start_time = high * 1000000  + low;
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint64_t low = inl(RTC_ADDR);
   uint64_t high = inl(RTC_ADDR + 4);
-  uptime->us = (high << 32) + low - start_time;
+  uptime->us = high * 1000000 + low - start_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
