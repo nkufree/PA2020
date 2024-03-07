e6d87e8abc6898f182f719e71831bed995e71c45
tracer-ics2020 <tracer@njuics.org>
2024-03-07 06:33:20 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  06:33:20 up  1:05,  0 users,  load average: 0.29, 0.18, 0.18

--- abstract-machine/am/src/nemu/ioe/audio.c
+++ abstract-machine/am/src/nemu/ioe/audio.c
@@ -37,7 +37,9 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
     int nwrite = len > buf_size ? buf_size : len;
     printf("1\n");
     while (inl(AUDIO_COUNT_ADDR) != 0);
+    printf("2\n");
     memcpy((uint32_t*)(uintptr_t)AUDIO_SBUF_ADDR, ctl->buf.start, nwrite);
+    printf("3\n");
     outl(AUDIO_COUNT_ADDR, nwrite);
     len -= nwrite;
     printf("10\n");
