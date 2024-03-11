cc62922e31a3993392a335e2938a0af41b1a17b3
tracer-ics2020 <tracer@njuics.org>
2024-03-11 05:36:55 +0000

    >  compile 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  05:36:54 up  3:41,  0 users,  load average: 0.53, 0.42, 0.28

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -6,7 +6,15 @@ void raise_intr(DecodeExecState *s, word_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+  uint32_t addr = cpu.idtr.base + 8 * NO;
+  uint32_t low = vaddr_read(addr, 4);
+  uint32_t high = vaddr_read(addr + 4, 4);
+  assert((high & 0x00008000) != 0); // p != 0
+  uint32_t dest_addr = (low & 0xffff) | (high & 0xffff0000);
+  rtl_push(s, &cpu.eflags);
+  rtl_push(s, &cpu.cs);
+  rtl_push(s, &ret_addr);
+  rtl_j(s, dest_addr);
 }
 
 void query_intr(DecodeExecState *s) {
