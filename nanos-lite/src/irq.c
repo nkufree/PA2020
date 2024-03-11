c89289c32c8d4f5e4e3942a2ef33a809b38af992
tracer-ics2020 <tracer@njuics.org>
2024-03-11 08:20:15 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  08:20:14 up  6:25,  0 users,  load average: 0.17, 0.24, 0.19

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_YIELD: printf("trap at yield\n");
     default: panic("Unhandled event ID = %d", e.event);
   }
 
