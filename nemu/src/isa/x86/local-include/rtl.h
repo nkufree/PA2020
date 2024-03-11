1ca6b99055b876d0bda6b4f5673eaa83079382c0
tracer-ics2020 <tracer@njuics.org>
2024-03-11 08:11:10 +0000

    >  compile 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  08:11:10 up  6:16,  0 users,  load average: 0.11, 0.11, 0.12

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -29,8 +29,9 @@ static inline def_rtl(sr, int r, const rtlreg_t* src1, int width) {
 static inline def_rtl(push, const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
+  *s0 = *src1;
   reg_l(R_ESP) -= 4;
-  rtl_sm(s, &reg_l(R_ESP), 0, src1, 4);
+  rtl_sm(s, &reg_l(R_ESP), 0, s0, 4);
 }
 
 static inline def_rtl(pop, rtlreg_t* dest) {
