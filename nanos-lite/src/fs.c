8fe9dd9e0467d5e0ffec98797e066fc1511b2bca
tracer-ics2020 <tracer@njuics.org>
2024-03-12 15:59:43 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:59:43 up 10:09,  0 users,  load average: 0.24, 0.25, 0.36

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -57,7 +57,8 @@ int fs_open(const char *pathname, int flags, int mode) {
 size_t fs_read(int fd, void *buf, size_t len) {
   Finfo* file = &file_table[fd];
   printf("read openoff: %d, len: %d\n", file->open_offset, len);
-  size_t read_len = file->size < len ? file->size : len;
+  size_t free_len = file->size - file->open_offset;
+  size_t read_len = free_len < len ? free_len : len;
   ramdisk_read(buf, file->disk_offset + file->open_offset, read_len);
   file->open_offset += read_len;
   return read_len;
