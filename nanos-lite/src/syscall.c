fa24fa211b0854d4e560726522289212ace4a8a3
tracer-ics2020 <tracer@njuics.org>
2024-03-12 09:16:22 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  09:16:21 up  3:26,  0 users,  load average: 0.32, 0.24, 0.28

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,12 @@
 #include <common.h>
 #include "syscall.h"
 void do_syscall(Context *c) {
-  printf("do syscall\n");
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
+  printf("do syscall, ID: %d\n", a[0]);
 
   switch (a[0]) {
     case 1: yield(); c->GPRx = 0; break;
