6f5d508f74c10805c4a205faf1a287d8a50437d8
tracer-ics2020 <tracer@njuics.org>
2024-03-12 08:11:27 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  08:11:26 up  2:21,  0 users,  load average: 0.51, 0.44, 0.30

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,33 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr __elf, *elf = &__elf;
+  Elf_Phdr __ph, *ph = &__ph;
+  ramdisk_read(elf, 0, sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)elf->e_ident == 0x464C457F);
+
+  uint32_t ph_index;
+  for(ph_index = 0; ph_index < elf->e_phnum; ph_index++)
+  {
+    size_t ph_off = elf->e_phoff + sizeof(Elf_Phdr) * ph_index;
+    ramdisk_read(ph, ph_off, sizeof(Elf_Phdr));
+    if(ph->p_type != PT_LOAD)
+    {
+      continue;
+    }
+    assert(ph->p_filesz <= ph->p_memsz);
+    uintptr_t start = ph->p_vaddr;
+    // 复制TEXT/DATA到指定位置
+    ramdisk_read((void*)start, ph->p_offset, ph->p_filesz);
+    // 构建BSS
+    start = ph->p_vaddr + ph->p_filesz;
+    memset((void*)start, 0, ph->p_memsz - ph->p_filesz);
+  }
+
+  return elf->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
