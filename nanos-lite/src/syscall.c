aede185b44440596e06b771995d3d17bb2589966
tracer-ics2020 <tracer@njuics.org>
2024-03-12 15:40:59 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:40:58 up  9:51,  0 users,  load average: 0.43, 0.57, 0.53

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,12 @@
 #include <common.h>
 #include "syscall.h"
+
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -21,11 +28,14 @@ void do_syscall(Context *c) {
         c->GPRx = a[3];
       }
       else
-        c->GPRx = 0;
+        c->GPRx = fs_write(a[1], (void*)(uintptr_t)a[2], a[3]);
       break;
     case SYS_brk: // a[1]: space
       c->GPRx = 0;
       break;
+    case SYS_read: c->GPRx = fs_read(a[1], (void*)(uintptr_t)a[2], a[3]); break;
+    case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
+    case SYS_close: c->GPRx = fs_close(a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
