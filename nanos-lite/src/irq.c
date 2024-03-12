22f05263a77b30c418d4b43f110475ef9e77f4a9
tracer-ics2020 <tracer@njuics.org>
2024-03-12 10:32:53 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  10:32:52 up  4:43,  0 users,  load average: 0.37, 0.31, 0.26

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
-  // printf("eip: %d\n", c->eip);
+  printf("eip: %d\n", c->eip);
   switch (e.event) {
     case EVENT_SYSCALL: do_syscall(c); break;
     case EVENT_YIELD: printf("trap at yield\n"); break;
