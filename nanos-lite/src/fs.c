b636103c6c7cc66cbc73426dee29cf050ed83409
tracer-ics2020 <tracer@njuics.org>
2024-03-12 15:30:20 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:30:19 up  9:40,  0 users,  load average: 1.35, 0.79, 0.50

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -3,12 +3,17 @@
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
+
 typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
@@ -33,4 +38,61 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+int fs_open(const char *pathname, int flags, int mode) {
+  int fd;
+  int file_num = sizeof(file_table) / sizeof(Finfo);
+  for(fd = 0; fd < file_num; fd++)
+  {
+    if(strcmp(pathname, file_table[fd].name) == 0)
+    {
+      file_table[fd].open_offset = 0;
+      return fd;
+    }
+  }
+  assert(0);
+}
+
+size_t fs_read(int fd, void *buf, size_t len) {
+  Finfo* file = &file_table[fd];
+  size_t read_len = file->size < len ? file->size : len;
+  ramdisk_read(buf, file->disk_offset + file->open_offset, read_len);
+  file->open_offset += read_len;
+  return read_len;
+}
+
+size_t fs_write(int fd, const void *buf, size_t len) {
+  Finfo* file = &file_table[fd];
+  size_t free_len = file->size - file->open_offset;
+  size_t write_len = free_len < len ? free_len : len;
+  ramdisk_write(buf, file->disk_offset + file->open_offset, write_len);
+  file->open_offset += write_len;
+  return write_len;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  Finfo* file = &file_table[fd];
+  switch (whence)
+  {
+  case SEEK_SET:
+    assert(offset >= 0 && offset <= file->size);
+    file->open_offset = offset;
+    break;
+  case SEEK_CUR:
+    assert((file->open_offset + offset) >=0 && (file->open_offset + offset) <= 0);
+    file->open_offset += offset;
+    break;
+  case SEEK_END:
+    assert((file->open_offset + offset) >=0 && (file->open_offset + offset) <= 0);
+    file->open_offset -= offset;
+    break;
+  default:
+    break;
+  }
+  return file->open_offset;
+}
+
+int fs_close(int fd) {
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,19 +10,25 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  int fd = fs_open(filename, 0, 0);
   Elf_Ehdr __elf, *elf = &__elf;
   Elf_Phdr __ph, *ph = &__ph;
-  ramdisk_read(elf, 0, sizeof(Elf_Ehdr));
+  fs_read(fd, elf, sizeof(Elf_Ehdr));
   assert(*(uint32_t *)elf->e_ident == 0x464C457F);
 
   uint32_t ph_index;
   for(ph_index = 0; ph_index < elf->e_phnum; ph_index++)
   {
     size_t ph_off = elf->e_phoff + sizeof(Elf_Phdr) * ph_index;
-    ramdisk_read(ph, ph_off, sizeof(Elf_Phdr));
+    fs_lseek(fd, ph_off, SEEK_SET);
+    fs_read(fd, ph, sizeof(Elf_Phdr));
     if(ph->p_type != PT_LOAD)
     {
       continue;
@@ -29,11 +36,13 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     assert(ph->p_filesz <= ph->p_memsz);
     uintptr_t start = ph->p_vaddr;
     // 复制TEXT/DATA到指定位置
-    ramdisk_read((void*)start, ph->p_offset, ph->p_filesz);
+    fs_lseek(fd, ph->p_offset, SEEK_SET);
+    fs_read(fd, (void*)start, ph->p_filesz);
     // 构建BSS
     start = ph->p_vaddr + ph->p_filesz;
     memset((void*)start, 0, ph->p_memsz - ph->p_filesz);
   }
+  fs_close(fd);
 
   return elf->e_entry;
 }
