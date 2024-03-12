cdb331c50e9ed5db1b4fde3455c327b158569b99
tracer-ics2020 <tracer@njuics.org>
2024-03-12 15:44:25 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:44:24 up  9:54,  0 users,  load average: 0.67, 0.69, 0.60

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -33,6 +33,7 @@ void do_syscall(Context *c) {
     case SYS_brk: // a[1]: space
       c->GPRx = 0;
       break;
+    case SYS_open: c->GPRx = fs_open((char*)(uintptr_t)a[1], a[2], a[3]); break;
     case SYS_read: c->GPRx = fs_read(a[1], (void*)(uintptr_t)a[2], a[3]); break;
     case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
     case SYS_close: c->GPRx = fs_close(a[1]); break;
