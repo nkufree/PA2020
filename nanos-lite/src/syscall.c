8d7f029a50cea6adfbf41775d0e60a880ec0edf7
tracer-ics2020 <tracer@njuics.org>
2024-03-12 13:01:05 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  13:01:04 up  7:11,  0 users,  load average: 0.27, 0.24, 0.19

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,6 +11,18 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_yield: yield(); c->GPRx = 0; break;
     case SYS_exit: halt(0); break;
+    case SYS_write: // a[1]: fd, a[2]: buf, a[3]: count
+      if(a[1] == 1 || a[1] == 2)
+      {
+        for(int i = 0; i < a[3]; i++)
+        {
+          putch(*(char*)(uintptr_t)(a[2] + i));
+        }
+        c->GPRx = a[3];
+      }
+      else
+        c->GPRx = 0;
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
