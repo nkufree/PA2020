8841b3a6adfd86e4492b74adc137bd27db3ce4f5
tracer-ics2020 <tracer@njuics.org>
2024-03-13 02:49:12 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  02:49:11 up  2:08,  0 users,  load average: 0.82, 0.58, 0.40

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -17,7 +17,7 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
   FILE * fd = fopen("/dev/events", "r+");
-  int ret = fprintf(fd, "%s",buf);
+  int ret = fscanf(fd, "%s",buf);
   fclose(fd);
   return ret;
 }
