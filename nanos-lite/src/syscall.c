61b7a0b43f564ead91c280ca022249a8b8589c0c
tracer-ics2020 <tracer@njuics.org>
2024-03-13 01:38:40 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  01:38:39 up 57 min,  0 users,  load average: 0.70, 0.46, 0.41

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,6 @@
 #include <common.h>
 #include "syscall.h"
+#include <sys/time.h>
 
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_read(int fd, void *buf, size_t len);
@@ -19,16 +20,7 @@ void do_syscall(Context *c) {
     case SYS_yield: yield(); c->GPRx = 0; break;
     case SYS_exit: halt(0); break;
     case SYS_write: // a[1]: fd, a[2]: buf, a[3]: count
-      // if(a[1] == 1 || a[1] == 2)
-      // {
-      //   for(int i = 0; i < a[3]; i++)
-      //   {
-      //     putch(*(char*)(uintptr_t)(a[2] + i));
-      //   }
-      //   c->GPRx = a[3];
-      // }
-      // else
-        c->GPRx = fs_write(a[1], (void*)(uintptr_t)a[2], a[3]);
+      c->GPRx = fs_write(a[1], (void*)(uintptr_t)a[2], a[3]);
       break;
     case SYS_brk: // a[1]: space
       c->GPRx = 0;
@@ -37,6 +29,12 @@ void do_syscall(Context *c) {
     case SYS_read: c->GPRx = fs_read(a[1], (void*)(uintptr_t)a[2], a[3]); break;
     case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
     case SYS_close: c->GPRx = fs_close(a[1]); break;
+    case SYS_gettimeofday: 
+      ; struct timeval* tv = (struct timeval*)a[1];
+      tv->tv_sec = io_read(AM_TIMER_UPTIME).us /1000000;
+      tv->tv_usec = io_read(AM_TIMER_UPTIME).us % 1000000;
+      c->GPRx = 0;
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
---
+++ navy-apps/tests/timer-test/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+int main()
+{
+  struct timeval last, curr;
+  gettimeofday(&last, NULL);
+  while(1)
+  {
+    gettimeofday(&curr, NULL);
+    int timegap = (curr.tv_sec - last.tv_sec) * 2 
+    + (curr.tv_usec - last.tv_usec) * 2 / 1000000;
+    if(timegap >= 0)
+    {
+      printf("hello\n");
+      last.tv_sec = curr.tv_sec;
+      last.tv_usec = curr.tv_usec;
+    }
+  }
+  return 0;
+}
