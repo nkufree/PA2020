0ff35cb6871288712db8d9cb3e3d3c3d9b11c228
tracer-ics2020 <tracer@njuics.org>
2024-03-13 05:31:08 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  05:31:07 up 11 min,  0 users,  load average: 0.64, 0.45, 0.31

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -26,7 +26,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE) return 0;
   sprintf(buf, "%s %s\n\0", ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
-  printf("name : %s\n", keyname[ev.keycode]);
+  printf("%s %s\n",ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
   return strlen((char*)buf);
 }
 
