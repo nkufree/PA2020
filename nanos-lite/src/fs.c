d301c36385c1cd61cf0de06012cc56e29e816f63
tracer-ics2020 <tracer@njuics.org>
2024-03-18 07:47:33 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  07:47:32 up  7:46,  0 users,  load average: 0.37, 0.24, 0.20

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -104,7 +104,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
   Finfo* file = &file_table[fd];
-  // printf("fd: %d, openoff: %d, off: %d, whe: %d\n", fd, file->open_offset, offset, whence);
+  printf("file: %s, openoff: %d, off: %d, whe: %d\n", file[fd].name, file->open_offset, offset, whence);
   switch (whence)
   {
   case SEEK_SET:
