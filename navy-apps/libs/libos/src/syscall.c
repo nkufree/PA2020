3256241a8da3f9435c6ac515cb59a7bf072662ef
tracer-ics2020 <tracer@njuics.org>
2024-03-20 13:25:46 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  13:25:45 up  9:47,  0 users,  load average: 0.53, 0.40, 0.31

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -94,7 +94,7 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz) {
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
-  return _syscall_(SYS_execve, fname, (intptr_t)argv, (intptr_t)envp);
+  return _syscall_(SYS_execve, (intptr_t)fname, (intptr_t)argv, (intptr_t)envp);
 }
 
 // Syscalls below are not used in Nanos-lite.
