311e1182c0719e3b57407745c03bfa66788fb1f7
tracer-ics2020 <tracer@njuics.org>
2024-03-20 07:59:47 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  07:59:46 up  4:21,  0 users,  load average: 0.58, 0.38, 0.30

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -68,6 +68,7 @@ void context_uload(PCB* pcb, const char *filename, char *const argv[], char *con
     envplen += strlen(envp[envc]) + 1;
     envc++;
   }
+  printf("argc: %d, envc: %d\n", argc, envc);
   void* ret = (void*)(uintptr_t)heap.end;
   ret -= argvlen + envplen + (argc + envc + 2) * sizeof(char*) + sizeof(int) + 12;
   *((int*)ret) = argc;
