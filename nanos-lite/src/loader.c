fb75fd8fe7508d7acbfd1d5bedf37c6b6c5e2128
tracer-ics2020 <tracer@njuics.org>
2024-03-21 07:54:22 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  07:54:21 up  2:57,  0 users,  load average: 0.20, 0.09, 0.02

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -69,13 +69,13 @@ void context_uload(PCB* pcb, const char *filename, char *const argv[], char *con
     envplen += strlen(envp[envc]) + 1;
     envc++;
   }
-  // printf("argc: %d, envc: %d\n", argc, envc);
+  Log("argc: %d, envc: %d\n", argc, envc);
   void* ret = new_page(4);
   ret -= argvlen + envplen + (argc + envc + 2) * sizeof(char*) + sizeof(int) + 12;
   *((int*)ret) = argc;
   char** argvp = ret + sizeof(int);
   char* string_area = ret + sizeof(int) + (argc + envc + 2) * sizeof(char*);
-  // printf("ret: %p, argvp: %p, string_area: %p\n", ret, argvp, string_area);
+  Log("ret: %p, argvp: %p, string_area: %p\n", ret, argvp, string_area);
   for(int i = 0; i < argc; i++)
   {
     strcpy(string_area, argv[i]);
