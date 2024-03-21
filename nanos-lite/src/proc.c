489021c97228351c3b3ca7ce0570a5aba3229190
tracer-ics2020 <tracer@njuics.org>
2024-03-21 13:54:08 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  13:54:07 up  8:57,  0 users,  load average: 1.30, 0.62, 0.42

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -49,8 +49,8 @@ void init_proc() {
   *a = 0;
   *b = 1;
   context_kload(&pcb[0], hello_fun, (void*)a);
-//   char* argv[] = {"/bin/busybox", "cat", "/share/games/bird/atlas.txt", NULL};
-  context_uload(&pcb[1], "/bin/nterm", NULL, NULL);
+  char* argv[] = {"/bin/nterm", NULL};
+  context_uload(&pcb[1], "/bin/nterm", argv, NULL);
   switch_boot_pcb();
 
 
