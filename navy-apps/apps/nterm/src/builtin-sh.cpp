fd71ff56dfccd2622c537c457a25d3ec508f2d82
tracer-ics2020 <tracer@njuics.org>
2024-03-21 13:57:29 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  13:57:28 up  9:00,  0 users,  load average: 0.92, 0.66, 0.47

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -23,7 +23,7 @@ static void sh_prompt() {
 }
 
 static void sh_handle_cmd(const char *cmd) {
-  char* argv[16] = {"/bin/busybox", NULL};
+  char* argv[16] = {NULL};
   int argc = 1;
   const char* start = cmd;
   int len = 0;
