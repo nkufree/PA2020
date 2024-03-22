ea092843e5497c3536b0ee98c82fa973c87b376c
tracer-ics2020 <tracer@njuics.org>
2024-03-22 12:21:49 +0000

    >  compile 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  12:21:48 up  5:57,  0 users,  load average: 0.26, 0.29, 0.35

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -61,9 +61,9 @@ void init_proc() {
 Context* schedule(Context *prev) {
   current->cp = prev;
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  if(current == &pcb_boot)
-    Log("schedule: current = boot");
-  else
-    Log("schedule: current = %d", current == &pcb[0] ? 0 : 1);
+//   if(current == &pcb_boot)
+//     Log("schedule: current = boot");
+//   else
+//     Log("schedule: current = %d", current == &pcb[0] ? 0 : 1);
   return current->cp;
 }
