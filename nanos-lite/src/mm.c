0fd0624dbd17b947a1affbf0011bd1c22273048d
tracer-ics2020 <tracer@njuics.org>
2024-03-22 13:18:34 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  13:18:34 up  6:54,  0 users,  load average: 0.50, 0.33, 0.26

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,6 +1,15 @@
 #include <memory.h>
+#include <proc.h>
+
+#define OFFSET 0xfff
+#define PGSIZE 4096
+#define page_begin_vaddr(A) ((void*)((uintptr_t)(A)&(~OFFSET)))
+#define page_end_vaddr(A) ((void*)(((uintptr_t)(A)&(~OFFSET))+PGSIZE))
+#define min(A,B) ((size_t)(A) < (size_t)(B)? (A):(B))
+#define vaddr_offset(A) ((uintptr_t)(A)&OFFSET)
 
 static void *pf = NULL;
+extern PCB *current;
 
 void* new_page(size_t nr_page) {
   void* start = pf;
@@ -20,8 +29,33 @@ void free_page(void *p) {
 }
 
 /* The brk() system call handler. */
-int mm_brk(uintptr_t brk) {
-  return 0;
+int mm_brk(uintptr_t brk)
+{
+  if (current->max_brk >= brk)
+  {
+    return 0;
+  }
+  else
+  {
+    void *vaddr = (void *)current->max_brk;
+    void *end = (void *)brk;
+    void *paddr = NULL;
+    uintptr_t len = (uintptr_t)min(page_end_vaddr(vaddr) - vaddr, end - vaddr);
+    if(len < PGSIZE) {
+      vaddr += len;
+    } else if(len == PGSIZE) {
+      assert(vaddr_offset(vaddr) == 0);
+    } else assert(0);
+    while((uintptr_t)vaddr < (uintptr_t)end)
+    {
+      len = (uintptr_t)min(page_end_vaddr(vaddr) - vaddr, end - vaddr);
+      paddr = new_page(1);
+      map(&current->as,page_begin_vaddr(vaddr),paddr,0);
+      vaddr += len;
+    }
+    current->max_brk = brk;
+  }
+  return 0; 
 }
 
 void init_mm() {
