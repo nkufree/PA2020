99c88b1e2a40f815cbda94fb6b4bf3a1965dfbaa
tracer-ics2020 <tracer@njuics.org>
2024-03-22 09:41:39 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  09:41:38 up  3:17,  0 users,  load average: 0.24, 0.50, 0.45

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -118,6 +118,7 @@ void context_uload(PCB* pcb, const char *filename, char *const argv[], char *con
   void* end = ret + 8 * PGSIZE;
   for(int i = 0; i < 8; i++) {
     map(&pcb->as, pcb->as.area.end - 1, end - i * PGSIZE, 0x7);
+    printf("vaddr: %p, paddr: %p\n", pcb->as.area.end - 1, end - i * PGSIZE);
   }
   ret -= argvlen + envplen + (argc + envc + 2) * sizeof(char*) + sizeof(int) + 12;
   *((int*)ret) = argc;
