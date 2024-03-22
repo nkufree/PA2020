473e73cdd6f6a3a0500787f0ddcb3e41d899d240
tracer-ics2020 <tracer@njuics.org>
2024-03-22 09:39:58 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  09:39:57 up  3:15,  0 users,  load average: 0.74, 0.66, 0.50

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -53,6 +53,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       last_page = new_page(1);
       fs_read(fd, last_page, len);
       map(&pcb->as, (void*)start, last_page, 0);
+      printf("vaddr: %p, len: %d, paddr: %p\n", start, len, last_page);
       start += len;
       build_size += len;
     }
@@ -67,6 +68,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     if(len < PGSIZE) {
       len = ph->p_memsz - build_size < PGSIZE ? ph->p_memsz - build_size : PGSIZE;
       memset((void*)start, 0, len);
+      printf("vaddr: %p, len: %d, paddr: %p\n", start, len, last_page);
       start += len;
       build_size += len;
     }
@@ -80,6 +82,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       last_page = new_page(1);
       memset(last_page, 0, len);
       map(&pcb->as, (void*)start, last_page, 0);
+      printf("vaddr: %p, len: %d, paddr: %p\n", start, len, last_page);
       start += len;
       build_size += len;
     }
