42b7762b0402619642321efdad83a9139d5c6c4a
tracer-ics2020 <tracer@njuics.org>
2024-03-23 18:15:35 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  18:15:31 up  3:41,  0 users,  load average: 1.02, 0.93, 0.62

--- navy-apps/tests/mytest/main.c
+++ navy-apps/tests/mytest/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+void print_argv(char* v) {
+    printf("%s\n", v);
+}
+
+int main(int      argc,
+   char    *argv[]) {
   int*a = (int*)malloc(10*sizeof(int));
-  printf("Hello, world!\n");
+  for(int i = 0; i < argc; i++)
+    print_argv(argv[i]);
   a[1] = 1;
   write(1, "Hello World!\n", 13);
   return 0;
 }
+
