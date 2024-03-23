e71a7582626a62d2c917929eaba70888c2281290
tracer-ics2020 <tracer@njuics.org>
2024-03-23 08:11:13 +0000

    >  compile 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  08:11:12 up  1:23,  0 users,  load average: 0.93, 0.48, 0.33

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -7,7 +7,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
   uint32_t offset = (uint32_t)vaddr & 0xfff;
   uint32_t pde = paddr_read((cpu.cr3 & 0xfffff000) | (dir << 2), 4);
   if((pde & 0x1) == 0) {
-    Log("vaddr: %x, pde = %x", vaddr, pde);
+    Log("cr3: %x, vaddr: %x, pde = %x",cpu.cr3, vaddr, pde);
     return MEM_RET_FAIL;
   }
   uint32_t pte_val = paddr_read((pde & 0xfffff000) | (page <<  2), 4);
