94e722389d01b7a5be21ce05fe2c91d73c8bcf2b
tracer-ics2020 <tracer@njuics.org>
2024-03-23 09:37:44 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  09:37:43 up  2:49,  0 users,  load average: 0.90, 0.51, 0.41

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -32,7 +32,7 @@ int mm_brk(uintptr_t brk){
   void* paddr = new_page(pages);
   for(int i = 0; i < pages; i++) {
     map(&current->as, (void*)(current->max_brk + i * PGSIZE), paddr + i * PGSIZE, 0);
-    Log("map vaddr: %p, paddr: %p", (void*)(current->max_brk + i * PGSIZE), paddr + i * PGSIZE);
+    // Log("map vaddr: %p, paddr: %p", (void*)(current->max_brk + i * PGSIZE), paddr + i * PGSIZE);
   }
   current->max_brk += pages * PGSIZE;
   return 0;
