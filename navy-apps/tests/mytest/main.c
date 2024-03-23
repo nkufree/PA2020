c57b823517f0857b8f621e839d310b942ba8be7e
tracer-ics2020 <tracer@njuics.org>
2024-03-23 15:36:42 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  15:36:41 up  1:02,  0 users,  load average: 0.42, 0.48, 0.43

--- navy-apps/tests/mytest/main.c
+++ navy-apps/tests/mytest/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 int main() {
-  printf("Hello, world!\n");
+//   printf("Hello, world!\n");
+  int*a = (int*)malloc(10*sizeof(int));
+  write(1, "Hello World!\n", 13);
   return 0;
 }
