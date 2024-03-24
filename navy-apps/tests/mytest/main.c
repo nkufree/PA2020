a1fe42d53ce87d5fb26e7f4878669d7046aa297a
tracer-ics2020 <tracer@njuics.org>
2024-03-24 03:22:19 +0000

    >  run 2110853 朱子轩 Linux 6c2034c917d2 5.15.146.1-microsoft-standard-WSL2 #3 SMP Tue Mar 5 00:51:27 CST 2024 x86_64 x86_64 x86_64 GNU/Linux  03:22:18 up 6 min,  0 users,  load average: 2.69, 2.03, 0.93

--- navy-apps/tests/mytest/main.c
+++ navy-apps/tests/mytest/main.c
@@ -8,6 +8,10 @@ void print_argv(char* v) {
 int main(int      argc,
    char    *argv[]) {
   int*a = (int*)malloc(10*sizeof(int));
+  for(int i = 0; i < 10; i++)
+    a[i] = i;
+  for(int i = 0; i < 10; i++)
+    printf("%d\n", a[i]);
   for(int i = 0; i < argc; i++)
     print_argv(argv[i]);
   a[1] = 1;
